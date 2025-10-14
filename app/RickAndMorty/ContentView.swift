c880c1cc08fc28b929783c6bf946c0058db57944
Carlos García <carlosypunto@gmail.com>
2025-10-14 13:29:28 +0200

    initial commit

---
+++ app/RickAndMorty/ContentView.swift
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 Carlos García Nieto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Carlos García Nieto. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
